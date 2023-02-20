c0fee7d45a9dc3c819a25a5dd94473362bcc6208
WendigoM3ris <merisstupar11@gmail.com>
2023-02-20 20:28:12 +0100

    Basic hooks added

---
+++ cypress/e2e/mocha-demo.cy.ts
@@ -0,0 +1,35 @@
+describe('template spec', () => {
+
+  before(()=>{
+    cy.log('Hello from the before hook');
+  });
+
+  after(()=>{
+    cy.log('Hello from the before hook');
+  });
+
+  beforeEach(()=>{
+    cy.log('Hello from the befor each hook');
+  });
+
+  afterEach(()=>{
+    cy.log('Hello from the after each hook');
+  });
+
+  it('Testcase #1', () => {
+    cy.log('Hello world');
+  });
+
+  it('Testcase #2', () => {
+    cy.log('Hello world');
+  });
+
+  it.only('Testcase #3', () => {
+    cy.log('Hello world');
+  });
+
+  it('Testcase #4', () => {
+    cy.log('Hello world');
+  });
+
+});
