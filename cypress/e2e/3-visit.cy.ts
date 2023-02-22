1b74e9e160764fbb1fc77d2e50ab403e5e396b67
WendigoM3ris <merisstupar11@gmail.com>
2023-02-22 16:44:23 +0100

    Added title validations

--- cypress/e2e/3-visit.cy.ts
+++ cypress/e2e/3-visit.cy.ts
@@ -6,8 +6,16 @@ describe('template spec', () => {
 
     it('Visit explanation text input', () => {
         cy.url().then((url)=>{
-            cy.log(`Printing the URL: ${url}`)
-        })
+            cy.log(`Printing the URL: ${url}`);
+            expect(url).to.contains('/textinput');
+        });
+    });
+
+    it('Title validation', ()=>{
+        cy.title().then((title)=>{
+            cy.log(title);
+            expect(title).to.be.equal('Text Input');
+        });
     });
 
   });
