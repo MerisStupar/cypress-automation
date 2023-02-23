5ed1ebc1f9e3257fa49f9396254f7fffab890791
WendigoM3ris <merisstupar11@gmail.com>
2023-02-23 20:01:54 +0100

    New test for basic alert on window

--- cypress/e2e/5-xpath-selectors.cy.ts
+++ cypress/e2e/5-xpath-selectors.cy.ts
@@ -4,7 +4,33 @@ describe("Locators", ()=>{
     });
 
     it('How to find element by its text', ()=>{
-        cy.xpath()
+        cy.xpath(`//*[text()="Correct variant is"]`).should(
+            'contain.text', 
+            'Correct');
+    });
+
+    it('Find the element by its attribute using xpath', ()=>{
+        cy.xpath(`//pre[@class=' language-html']`).should(
+            'contain.text', 
+            'button');
+    });
+
+    it('Find by class (middle and spaces)', ()=>{
+        cy.xpath(`//button[contains(concat(' ', normalize-space(@class), ' '), ' btn-warning')]`)
+        .should('have.css', 'background-color', 'rgb(255, 193, 7)')
+    });
+
+    it.only('Find button then click to show ALERT MESSAGE - example', ()=>{
+        cy.xpath(`//button[contains(concat(' ', normalize-space(@class), ' '), ' btn-primary')]`)
+        .should('have.css', 'background-color', 'rgb(0, 123, 255)')
+        .contains("Button")
+        .click()
+
+        //Getting the alert
+        cy.on('window:alert', (alret)=>{
+            //assertion
+            expect(alret).to.contain('Primary button pressed');
+        });
     });
 
 });
