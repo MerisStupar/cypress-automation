c422c4efd36d555d49b41577023a279d5f717dc2
WendigoM3ris <merisstupar11@gmail.com>
2023-02-26 22:15:03 +0100

    added environments

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,6 +6,17 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    env: {
+      demoVar: "Hello from the Cypress.Config.Ts",
+      demoQA: "https://demoqa.com/",
+      theInternetHeroku: "https://the-internet.herokuapp.com/",
+      //https://www.globalsqa.com/angularjs-protractor-practice-site/
+      angular: "https://www.globalsqa.com"
+
+
+    }
   },
   pageLoadTimeout: 3000,
+  viewportHeight: 1000,
+  viewportWidth: 1400
 });
---
+++ cypress/e2e/8-env-vars.cy.ts
@@ -0,0 +1,7 @@
+describe('Environment Variable Demo', () => {
+    
+    it('Demo', () => {
+        cy.log(`Printing Environment Variable Value: ${Cypress.env("demoVar")}`)
+    });
+
+});
