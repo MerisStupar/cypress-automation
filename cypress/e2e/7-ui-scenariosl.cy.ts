495b049265a2c3fa4af1652d1536b4beaf1ed8da
WendigoM3ris <merisstupar11@gmail.com>
2023-02-26 10:22:12 +0100

    Aded test with for loop for dynamic table

--- cypress/e2e/7-ui-scenariosl.cy.ts
+++ cypress/e2e/7-ui-scenariosl.cy.ts
@@ -46,12 +46,41 @@ describe('Dynamic table challange', () => {
                 chromeRowValues.push($cell.next().next().next().next().text());
                 cy.log("Chrome row values", chromeRowValues);
 
-                chromeRowValues.forEach((value)=>{
+/*                 chromeRowValues.forEach((value)=>{
                     if(value.includes('%')){
                         cy.log(value);
                         cy.get(`p[class="bg-warning"]`).should('have.text', `Chrome CPU: ${value}`);
                     }
-                })
+                }) */
+            }
+        });
+    });
+
+
+    it.only('Chrome CPU Test - Dynamic table Example  - FOR LOOP', () => {
+        cy.get(`div[role="row"] span`).each(($cell) =>{
+        
+            if($cell.text().includes('Chrome')){
+                cy.log(`I have found the ${$cell.text()} row !`);
+                
+                const chromeRowValues: string [] = [];
+
+                for(let i=0; i<=4; i++){
+                    const cellValue = $cell.nextAll().eq(i).text();  //.nextAll() - added to go to the limit of the for loop
+                    if(cellValue){
+                        chromeRowValues.push(cellValue);
+                    } else{
+                        cy.log(`Error: Could not find value for cell ${i} in Chrome row`)
+                    }
+                }
+                cy.log("Chrome row values", chromeRowValues);
+
+               chromeRowValues.forEach((value)=>{
+                    if(value.includes('%')){
+                        cy.log(value);
+                        cy.get(`p[class="bg-warning"]`).should('have.text', `Chrome CPU: ${value}`);
+                    }
+                }) 
             }
         });
     });
