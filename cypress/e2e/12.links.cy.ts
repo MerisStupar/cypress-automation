d05f2b6ff861db7bd4988a76b137cbec50a593d8
WendigoM3ris <merisstupar11@gmail.com>
2023-02-28 14:11:20 +0100

    Added test for several link api test

--- cypress/e2e/12.links.cy.ts
+++ cypress/e2e/12.links.cy.ts
@@ -1,4 +1,4 @@
-describe('Dealing with links', () => {
+describe.skip('Dealing with links', () => {
     
     beforeEach(()=>{
         cy.visit(`${Cypress.env("demoQA")}/links`, {timeout: 5000});
@@ -21,12 +21,12 @@ describe('Dealing with links', () => {
 describe('Intercepting API(SPYING) request after clicking on the button', () => {
     
     beforeEach(()=>{
-        cy.visit(`${Cypress.env("demoQA")}/links`, {timeout: 5000});
+        cy.visit(`${Cypress.env("demoQA")}/links`, {timeout: 20000});
 
     });
 
 
-    it.only('Intercepting CREATED link  - CREATED', () => {
+    it('Intercepting CREATED link  - CREATED', () => {
 
         cy.intercept('GET', `${Cypress.env("demoQA")}/created`).as('linkStatus')
 
@@ -48,7 +48,8 @@ describe('Intercepting API(SPYING) request after clicking on the button', () => 
 
         cy.wait('@no-content').then((request)=>{
             cy.log('This is the intercepted request', request);
-       
+            expect(request.response.statusCode).to.be.equal(204);
+            expect(request.response.statusMessage).to.be.equal('No Content');
         });
     });
 
@@ -61,7 +62,66 @@ describe('Intercepting API(SPYING) request after clicking on the button', () => 
 
         cy.wait('@moved').then((request)=>{
             cy.log('This is the intercepted request', request);
-       
+            expect(request.response.statusCode).to.be.equal(301)
+            expect(request.response.statusMessage).to.be.equal('Moved Permanently');
+        });
+    });
+
+    it('Intercepting BAD REQUEST link - BAD REQUEST', () => {
+        
+        cy.intercept('GET', `${Cypress.env("demoQA")}/bad-request`).as("bad-request");
+
+        cy.get('#bad-request').click();
+        cy.get('#linkResponse').should('have.text', "Link has responded with staus 400 and status text Bad Request");
+
+        cy.wait('@bad-request').then((request)=>{
+            cy.log('This is the intercepted request', request);
+            expect(request.response.statusCode).to.be.equal(400)
+            expect(request.response.statusMessage).to.be.equal('Bad Request');
+        });
+    });
+
+    it('Intercepting UNAUTHORIZED link - UNAUTHORIZED', () => {
+        
+        cy.intercept('GET', `${Cypress.env("demoQA")}/unauthorized`).as("unauthorized");
+
+        cy.get('#unauthorized').click();
+        cy.get('#linkResponse').should('have.text', "Link has responded with staus 401 and status text Unauthorized");
+
+        cy.wait('@unauthorized').then((request)=>{
+            cy.log('This is the intercepted request', request);
+            expect(request.response.statusCode).to.be.equal(401)
+            expect(request.response.statusMessage).to.be.equal('Unauthorized');
+        });
+    });
+
+    it('Intercepting FROBIDDEN link - FROBIDDEN', () => {
+        
+        cy.intercept('GET', `${Cypress.env("demoQA")}/unauthorized`).as("unauthorized");
+
+        cy.get('#unauthorized').click();
+        cy.get('#linkResponse').should('have.text', "Link has responded with staus 401 and status text Unauthorized");
+
+        cy.wait('@unauthorized').then((request)=>{
+            cy.log('This is the intercepted request', request);
+            expect(request.response.statusCode).to.be.equal(401)
+            expect(request.response.statusMessage).to.be.equal('Unauthorized');
+        });
+    });
+
+
+    
+    it('Intercepting NOT FOUND link - NOT FOUND', () => {
+        
+        cy.intercept('GET', `${Cypress.env("demoQA")}/invalid-url`).as("invalid-url");
+
+        cy.get('#invalid-url').click();
+        cy.get('#linkResponse').should('have.text', "Link has responded with staus 404 and status text Not Found");
+
+        cy.wait('@invalid-url').then((request)=>{
+            cy.log('This is the intercepted request', request);
+            expect(request.response.statusCode).to.be.equal(404)
+            expect(request.response.statusMessage).to.be.equal('Not Found');
         });
     });
 
