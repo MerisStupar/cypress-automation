d3870b9af702f9640ccf39f2c974ed5bce3bc93b
WendigoM3ris <merisstupar11@gmail.com>
2023-03-01 15:40:52 +0100

    added test for broken images with new sample of test with counter

--- cypress/e2e/13.broken.images.cy.ts
+++ cypress/e2e/13.broken.images.cy.ts
@@ -1,6 +1,6 @@
 let image:HTMLImageElement;
 
-describe('Broken images', () => {
+describe.skip('Broken images', () => {
     beforeEach(()=>{
         cy.visit(`${Cypress.env("demoQA")}/broken`, {timeout: 20000});
     });
@@ -20,5 +20,74 @@ describe('Broken images', () => {
             expect(image.naturalWidth).to.be.greaterThan(0);
                  cy.log('This is the image yielded', $img);
           });
+    });
+});
+
+describe('Broken images in the list - .first()', () => {
+    beforeEach(()=>{
+        cy.visit(`${Cypress.env("theInternetHeroku")}/broken_images`, {timeout: 20000});
+    });
+
+    it('Broken Image Detected - first image in the list', () => {
+        cy.get(`div.example >img`)
+        .first()
+        .should('be.visible')
+        .then(($img)=>{
+            image =$img[0] as unknown as HTMLImageElement;
+            expect(image.naturalWidth).to.be.eq(0);
+            cy.log(`Image yielded info`, $img);
+        });
+    });
+
+
+    it('Valid Image Detected - .last()', () => {
+        cy.get(`div.example >img`)
+        .last()
+        .should('be.visible')
+        .then(($img)=>{
+            image =$img[0] as unknown as HTMLImageElement;
+            expect(image.naturalWidth).to.be.greaterThan(0);
+            cy.log(`Image yielded info`, $img);
+        });
+    });
+
+    it('Valid Image Detected - with if condition', () => {
+        cy.get(`div.example >img`).each(($img)=>{
+
+        const brokenImagesNum = 0;
+        image=$img[0] as unknown as HTMLImageElement;
+
+            if(image.naturalHeight > 0){
+               cy.log(`I've found the valid image in the list`, image, $img)
+            }else{
+                cy.log(`There is ${brokenImagesNum} images with broken image`, image);
+            }
+      /*       cy.log(`Number of the images are: `, $img); */
+        });
+    });
+
+    it('Valid Image Detected & Broken image - with counter ', () => {
+
+        let brokenImages=0; // Initialize counter for broken images
+
+        cy.get(`div.example >img`).each(($img)=>{
+
+        image=$img[0] as unknown as HTMLImageElement;
+
+            if(image.naturalHeight > 0){
+               cy.log(`I've found the valid image in the list`, image);
+            }
+            else if(image.naturalHeight === 0){
+            cy.log(`Found the broken image in the list`, image);
+            cy.wrap($img)
+            .should('have.attr', 'src').then((src)=>{
+                cy.log(`Image source with broken image: ${src}`);
+            });
+            brokenImages++;
+            }
+        }).then(()=>{
+            cy.log(`Total number of broken images is: ${brokenImages}`);
+        });
+        
     });
 });
