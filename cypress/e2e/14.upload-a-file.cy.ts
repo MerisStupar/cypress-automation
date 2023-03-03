f9c33994a463a20cb38344362465dbde6cf22f31
WendigoM3ris <merisstupar11@gmail.com>
2023-03-03 14:32:03 +0100

    added test for file upload on herokuapp

--- cypress/e2e/14.upload-a-file.cy.ts
+++ cypress/e2e/14.upload-a-file.cy.ts
@@ -1,4 +1,4 @@
-describe('Upload a file in the QA Demo site', () => {
+describe.skip('Upload a file in the QA Demo site', () => {
     
     beforeEach(()=>{
         cy.visit(`${Cypress.env("demoQA")}/upload-download`, {timeout: 20000});
@@ -6,7 +6,26 @@ describe('Upload a file in the QA Demo site', () => {
 
 
     it('Upload the file', () => {
-        cy.get()
+        cy.get('input[id="uploadFile"]').attachFile('example.json');
+        cy.get('p#uploadedFilePath').should('contain', "example.json");
     });
 
+});
+
+
+describe('Upload a file in the Heroku App', () => {
+    
+    beforeEach(()=>{
+        cy.visit(`${Cypress.env("theInternetHeroku")}/upload`, {timeout: 20000});
+    });
+
+
+    
+    it('Upload the file on Heroku App', () => {
+       cy.get('input#file-upload').attachFile('example.json');
+       cy.get('input#file-submit').click();
+       cy.url().should('eql', "https://the-internet.herokuapp.com/upload");
+       cy.get('h3').contains('File Uploaded!');
+       cy.get(`div > div[id="uploaded-files"]`).should('contain', 'example.json');
+    });
 });
