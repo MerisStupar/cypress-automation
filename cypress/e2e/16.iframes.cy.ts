b19454ddaa49fc7f0932b06f1902e07e7219b7e1
WendigoM3ris <merisstupar11@gmail.com>
2023-03-06 15:38:32 +0100

    Main: Added iframe test for typing and bolding text in iframe

--- cypress/e2e/16.iframes.cy.ts
+++ cypress/e2e/16.iframes.cy.ts
@@ -1,6 +1,6 @@
-describe('IFrame example', () => {
+describe.skip('IFrame example', () => {
     beforeEach(()=>{
-        cy.visit(`${Cypress.env("demoQA")}/nestedframes`);
+        cy.visit(`${Cypress.env("demoQA")}/nestedframes`, {timeout: 20000});
     });
 
     it('Simple and nested iframe', () => {
@@ -12,5 +12,38 @@ describe('IFrame example', () => {
                 cy.wrap($childBody).find('p').should('have.text', "Child Iframe");
             })
         });
+    });
+});
+
+describe('IFrame example', () => {
+    beforeEach(()=>{
+        cy.visit(`${Cypress.env("theInternetHeroku")}/iframe`, {timeout: 15000});
+    });
+
+    it('Simple and nested iframe', () => {
+        cy.get(`#mce_0_ifr`).then(($iframe)=>{
+            const $body = $iframe.contents().find('body');
+            cy.wrap($body).find('p')
+            .type("{selectAll}{del}Hello World! How are you?");
+        });
+        cy.get(`#mce_0_ifr`).then(($iframe)=>{
+            const $body = $iframe.contents().find('body');
+            cy.wrap($body).find('p').should('have.text', "Hello World! How are you?");
+        });
+
+        cy.get(`#mce_0_ifr`).then(($iframe)=>{
+            const $body = $iframe.contents().find('body');
+            cy.wrap($body).find('p').type("{selectAll}")
+            
+            //Bolding the input text
+            cy.get(`.tox-toolbar__group:nth-of-type(3) span.tox-tbtn__icon-wrap:first-of-type`).eq(0)
+            .click()
+            cy.wrap($body).find('p').type("{esc}")
+
+
+        });
+
+      
+       
     });
 });
