fd6c191144219758180a9ab8af2c265666f4c083
WendigoM3ris <merisstupar11@gmail.com>
2023-03-08 10:36:02 +0100

    Main: Created new test file

---
+++ cypress/e2e/18.accordion.cy.ts
@@ -0,0 +1,12 @@
+describe('Accordian', () => {
+    
+    beforeEach(()=>{
+        cy.visit(`${Cypress.env("demoQA")}/accordian`, {timeout: 20000});
+    });
+
+    it('Default Accordion validation example', () => {
+        
+    });
+
+
+});
