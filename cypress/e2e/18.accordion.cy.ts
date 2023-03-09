b69a7073d5e8518be7b71e751da1a9f657017579
WendigoM3ris <merisstupar11@gmail.com>
2023-03-09 22:45:22 +0100

    Main: Finished accordion.cy.ts test

--- cypress/e2e/18.accordion.cy.ts
+++ cypress/e2e/18.accordion.cy.ts
@@ -5,7 +5,12 @@ describe('Accordian', () => {
     });
 
     it('Default Accordion validation example', () => {
-        
+        cy.get(`#section2Heading`).click();
+        //Cheking if the first desc was hidden correctly.
+        cy.get(`#section1Heading`).next().should('have.css', 'display', 'none'); 
+        //Cheking if the second desc was diplayed
+        cy.get(`#section2Heading`).next().should('have.css', 'display', 'block')
+        .and('have.class', 'show'); 
     });
 
 
