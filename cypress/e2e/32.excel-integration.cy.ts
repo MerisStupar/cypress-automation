a7d2177abacacbd19913c67dfa86040017dfc6d4
WendigoM3ris <merisstupar11@gmail.com>
2023-03-17 19:48:00 +0100

    Main: Finished about excel test examples

--- cypress/e2e/32.excel-integration.cy.ts
+++ cypress/e2e/32.excel-integration.cy.ts
@@ -5,8 +5,26 @@ describe('Excel playground', () => {
         cy.parseXlsx("cypress/fixtures/excelData.xlsx").then((jsonData)=>{
             //Check what it is returning
             cy.log(jsonData);
+            cy.log(jsonData[0].data);
+
+            let data: [] = jsonData[0].data;
+            // let dataLenght = data.length;
+
+
+            data.forEach((row)=>{
+                cy.log(row[0]);
+                cy.log(row[1]);
+            });
+
+            data.forEach((row)=>{
+                cy.writeFile('cypress/fixtures/excelData.json',{
+                    username: row[0],
+                    password: row[1],
+                });
+            })
+
         })
 
     });
 
-});
+});   
