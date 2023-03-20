e3f52c6386d6f08c81bfa4f6b310498d03b0c8de
WendigoM3ris <merisstupar11@gmail.com>
2023-03-20 14:00:41 +0100

    Main: New test for DB

--- cypress/e2e/33.database-testing.cy.ts
+++ cypress/e2e/33.database-testing.cy.ts
@@ -10,17 +10,38 @@ describe('MySQL Database testing', () => {
       });
 
     
-      it.only('Inserting into table MOVIE', () => {
-        
+      it("Inserting into table MOVIE", () => {
         cy.task(
           "queryDb",
           `INSERT INTO movies VALUES
           ("Joker", "Psycholohical Thriller", "Todd Phillips", 2019),
           ("Peaky Blinders", "Series", "Todd Phillips", 2015)`
-        ).then((result: any)=>{
+        ).then((result: any) => {
           expect(result.affectedRows).to.equal(2);
-          expect(result.message).to.be.equal("&Records: 2  Duplicates: 0  Warnings: 0");
-        })
+          expect(result.message).to.be.equal(
+            "&Records: 2  Dupliscates: 0  Warnings: 0"
+          );
+        });
+      });
+
+      it.only('Select all movies', () => {
+        
+        cy.task('queryDb', `SELECT * FROM movies; `).then((result: any)=>{
+          cy.log("First row validation: ").then(()=>{
+              expect(result[0].title).to.be.equal("Joker");
+              expect(result[0].genre).to.be.equal("Psycholohical Thriller");
+              expect(result[0].director).to.be.equal("Todd Phillips");
+              expect(result[0].release_year).to.be.eql(2019)
+          });
+          cy.log("Second row validation: ").then(()=>{
+            expect(result[1].title).to.be.equal("Peaky Blinders");
+            expect(result[1].genre).to.be.equal("Series");
+            expect(result[1].director).to.be.equal("Todd Phillips");
+            expect(result[1].release_year).to.be.eql(2015);
+        });
+        });
+
+
       });
     
 
