bcbb7fa3e76e224dfeadec1884dfd546e4868737
WendigoM3ris <merisstupar11@gmail.com>
2023-03-21 13:53:26 +0100

    Main: Added new test for mocking and added json file

---
+++ cypress/e2e/35.mocking.cy.ts
@@ -0,0 +1,12 @@
+describe('Mocking an API request', () => {
+    
+    beforeEach(()=>{
+        cy.visit(`${Cypress.env("demoQA")}/login`);
+    });
+
+
+    it('Mocking data', () => {
+        
+    });
+
+});
