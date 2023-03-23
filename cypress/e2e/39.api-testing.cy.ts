2d87b89145ade304f81a2e03688b6f12389d12d9
WendigoM3ris <merisstupar11@gmail.com>
2023-03-23 21:03:03 +0100

    Main: new test added

--- cypress/e2e/39.api-testing.cy.ts
+++ cypress/e2e/39.api-testing.cy.ts
@@ -9,19 +9,41 @@ describe('Basic API test example', () => {
 
     });
 
-    it('Post a new resource', () => {
+    it('Post a new resource - POST METHOD', () => {
         
         cy.request('POST', 'https://jsonplaceholder.typicode.com/posts', {
+
             title: 'Title #101',
             body: 'Post #101',
             userId: 101
         }).then((respone)=>{
             cy.log('Response: ', respone);
+            //Asserting the response from the POST request
             expect(respone.status).to.be.eq(201);
             expect(respone.statusText).to.be.equal('Created');
+            expect(respone.body.title).to.be.equal('Title #101');
+        });
+    });
+
+    it.only('Update a new resource - PUT METHOD', ()=>{
+
+        cy.request('PUT', 'https://jsonplaceholder.typicode.com/posts/1', {
+
+            id: 1,
+            title: "Test - PUT",
+            body: "Test - BODY",
+            userId: 101
+
+        })
+        .then((response)=>{
+            cy.log('Response: ', response);
+            expect(response.status).to.be.equal(200);
         });
 
 
     });
+
+
+
 
 });
